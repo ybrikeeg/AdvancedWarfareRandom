f7a084e20945f434998d8356caa44431b10201e7
ybrikeeg <kirbygee@stanford.edu>
2014-11-23 13:24:34 -0800

    Initial Commit

---
+++ AWRandom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AWRandom
+//
+//  Created by Kirby Gee on 11/23/14.
+//  Copyright (c) 2014 Kirby Gee - Stanford Univeristy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AWRandom/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AWRandom
+//
+//  Created by Kirby Gee on 11/23/14.
+//  Copyright (c) 2014 Kirby Gee - Stanford Univeristy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+   @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+   }
+}
